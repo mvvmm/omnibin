5ba936aa533e327da81a90a10f8c2a78fae4c25c
mvm <root.mvm@gmail.com>
2025-08-04 17:15:04 -0500

    redirect to bin after login

--- web/src/lib/auth0.ts
+++ web/src/lib/auth0.ts
@@ -1,4 +1,6 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
 // Initialize the Auth0 client
-export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+	signInReturnToPath: "/bin",
+});
