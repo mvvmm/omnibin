d748ea1d67dec7f8fcb245667277e685829d95f3
mvm <root.mvm@gmail.com>
2025-11-05 18:05:47 -0600

    web: make blobs work better for mobile

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -50,89 +50,139 @@ export default function RootLayout({
 				<ThemeProvider>
 					{/* Fixed background elements that cover the entire viewport */}
 					<div className="fixed inset-0 pointer-events-none">
-						{/* Blob 1 - Top Left */}
-						<div
-							className="absolute blur-[60px] sm:blur-[100px] md:blur-[200px]"
-							style={{
-								backgroundColor: "var(--blob-1)",
-								opacity: "var(--blob-opacity)",
-								top: "-8%",
-								left: "-6%",
-								width: "44rem",
-								height: "38rem",
-								borderRadius: "60% 40% 30% 70% / 60% 30% 70% 40%",
-							}}
-						/>
+						{/* Mobile Blobs */}
+						<div className="md:hidden absolute inset-0">
+							{/* Mobile Blob 1 - Blue (Top Left) */}
+							<div
+								className="absolute blur-[60px] sm:blur-[100px]"
+								style={{
+									backgroundColor: "var(--blob-5)",
+									opacity: "var(--blob-opacity)",
+									borderRadius: "70% 30% 50% 50% / 40% 60% 50% 60%",
+									left: "-20%",
+									width: "42rem",
+									height: "38rem",
+									transform: "translateY(-50%)",
+								}}
+							/>
 
-						{/* Blob 2 - Bottom Right */}
-						<div
-							className="absolute blur-[60px] sm:blur-[100px] md:blur-[200px]"
-							style={{
-								backgroundColor: "var(--blob-2)",
-								opacity: "var(--blob-opacity)",
-								bottom: "-12%",
-								right: "-8%",
-								width: "48rem",
-								height: "42rem",
-								borderRadius: "30% 60% 70% 40% / 50% 60% 30% 60%",
-							}}
-						/>
+							{/* Mobile Blob 2 - Cyan (Middle Right) */}
+							<div
+								className="absolute blur-[60px] sm:blur-[100px]"
+								style={{
+									backgroundColor: "var(--blob-6)",
+									opacity: "var(--blob-opacity)",
+									borderRadius: "50% 50% 40% 60% / 50% 50% 60% 40%",
+									bottom: "0%",
+									right: "-14%",
+									width: "42rem",
+									height: "32rem",
+									transform: "translateY(-50%)",
+								}}
+							/>
 
-						{/* Blob 3 - Top Center */}
-						<div
-							className="absolute blur-[60px] sm:blur-[100px] md:blur-[200px]"
-							style={{
-								backgroundColor: "var(--blob-3)",
-								opacity: "var(--blob-opacity)",
-								top: "-5%",
-								left: "42%",
-								width: "36rem",
-								height: "40rem",
-								borderRadius: "50% 50% 50% 50% / 60% 40% 60% 40%",
-							}}
-						/>
+							{/* Mobile Blob 3 - Pink (Bottom Center) */}
+							<div
+								className="absolute blur-[60px] sm:blur-[100px]"
+								style={{
+									backgroundColor: "var(--blob-4)",
+									opacity: "var(--blob-opacity)",
+									bottom: "2%",
+									left: "-20%",
+									width: "34rem",
+									height: "42rem",
+									borderRadius: "40% 60% 50% 60% / 70% 30% 50% 50%",
+									transform: "translateY(50%)",
+								}}
+							/>
+						</div>
 
-						{/* Blob 4 - Bottom Center */}
-						<div
-							className="absolute blur-[60px] sm:blur-[100px] md:blur-[200px]"
-							style={{
-								backgroundColor: "var(--blob-4)",
-								opacity: "var(--blob-opacity)",
-								bottom: "-10%",
-								left: "48%",
-								width: "40rem",
-								height: "46rem",
-								borderRadius: "40% 60% 50% 60% / 70% 30% 50% 50%",
-							}}
-						/>
+						{/* Desktop Blobs */}
+						<div className="hidden md:block absolute inset-0">
+							{/* Desktop Blob 1 - Top Left */}
+							<div
+								className="absolute blur-[200px]"
+								style={{
+									backgroundColor: "var(--blob-1)",
+									opacity: "var(--blob-opacity)",
+									top: "-8%",
+									left: "-6%",
+									width: "44rem",
+									height: "38rem",
+									borderRadius: "60% 40% 30% 70% / 60% 30% 70% 40%",
+								}}
+							/>
 
-						{/* Blob 5 - Bottom Left */}
-						<div
-							className="absolute blur-[60px] sm:blur-[100px] md:blur-[200px]"
-							style={{
-								backgroundColor: "var(--blob-5)",
-								opacity: "var(--blob-opacity)",
-								bottom: "-10%",
-								left: "-6%",
-								width: "38rem",
-								height: "44rem",
-								borderRadius: "70% 30% 50% 50% / 40% 60% 50% 60%",
-							}}
-						/>
+							{/* Desktop Blob 2 - Bottom Right */}
+							<div
+								className="absolute blur-[200px]"
+								style={{
+									backgroundColor: "var(--blob-2)",
+									opacity: "var(--blob-opacity)",
+									bottom: "-12%",
+									right: "-8%",
+									width: "48rem",
+									height: "42rem",
+									borderRadius: "30% 60% 70% 40% / 50% 60% 30% 60%",
+								}}
+							/>
 
-						{/* Blob 6 - Top Right */}
-						<div
-							className="absolute blur-[60px] sm:blur-[100px] md:blur-[200px]"
-							style={{
-								backgroundColor: "var(--blob-6)",
-								opacity: "var(--blob-opacity)",
-								top: "18%",
-								right: "-6%",
-								width: "42rem",
-								height: "34rem",
-								borderRadius: "50% 50% 40% 60% / 50% 50% 60% 40%",
-							}}
-						/>
+							{/* Desktop Blob 3 - Top Center */}
+							<div
+								className="absolute blur-[200px]"
+								style={{
+									backgroundColor: "var(--blob-3)",
+									opacity: "var(--blob-opacity)",
+									top: "-5%",
+									left: "42%",
+									width: "36rem",
+									height: "40rem",
+									borderRadius: "50% 50% 50% 50% / 60% 40% 60% 40%",
+								}}
+							/>
+
+							{/* Desktop Blob 4 - Bottom Center */}
+							<div
+								className="absolute blur-[200px]"
+								style={{
+									backgroundColor: "var(--blob-4)",
+									opacity: "var(--blob-opacity)",
+									bottom: "-10%",
+									left: "48%",
+									width: "40rem",
+									height: "46rem",
+									borderRadius: "40% 60% 50% 60% / 70% 30% 50% 50%",
+								}}
+							/>
+
+							{/* Desktop Blob 5 - Bottom Left */}
+							<div
+								className="absolute blur-[200px]"
+								style={{
+									backgroundColor: "var(--blob-5)",
+									opacity: "var(--blob-opacity)",
+									bottom: "-10%",
+									left: "-6%",
+									width: "38rem",
+									height: "44rem",
+									borderRadius: "70% 30% 50% 50% / 40% 60% 50% 60%",
+								}}
+							/>
+
+							{/* Desktop Blob 6 - Top Right */}
+							<div
+								className="absolute blur-[200px]"
+								style={{
+									backgroundColor: "var(--blob-6)",
+									opacity: "var(--blob-opacity)",
+									top: "18%",
+									right: "-6%",
+									width: "42rem",
+									height: "34rem",
+									borderRadius: "50% 50% 40% 60% / 50% 50% 60% 40%",
+								}}
+							/>
+						</div>
 
 						{/* Grid */}
 						<div
