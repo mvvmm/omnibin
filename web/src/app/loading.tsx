4436c70240cebdaa19ebc1dff8072c2c0bdb0cdc
mvm <root.mvm@gmail.com>
2025-11-07 17:28:44 -0600

    web: try this index route loading animation

---
+++ web/src/app/loading.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export default function Loading() {
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-center">
+			<div className="relative w-48 animate-grow">
+				{/* Glassy/transparent base - grayscale */}
+				<Image
+					src="/binboy.png"
+					alt="binboy logo"
+					width={200}
+					height={200}
+					priority
+					className="h-auto w-48"
+					style={{
+						filter: "grayscale(100%) opacity(0.15) blur(2px)",
+					}}
+				/>
+				{/* Colored fill overlay */}
+				<div
+					className="absolute top-0 left-0 w-full h-full overflow-hidden animate-fill-up"
+					style={{ clipPath: "inset(100% 0% 0% 0%)" }}
+				>
+					<Image
+						src="/binboy.png"
+						alt="binboy logo"
+						width={200}
+						height={200}
+						priority
+						className="h-auto w-48"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
